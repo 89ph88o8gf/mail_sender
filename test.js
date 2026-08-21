e17e2706273a39e26fffe5d0dd463a8e70ce2f5b
89ph88o8gf <mihajlovicpavle702@gmail.com>
2026-08-21 18:47:09 +0200

    Replace nodemailer with Brevo API for email sending

--- test.js
+++ test.js
@@ -1,24 +1,31 @@
-const nodemailer = require("nodemailer");
+const Brevo = require("@getbrevo/brevo");
 
-const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "b64861001@smtp-brevo.com",
-        pass: "bskbQFr1hwpWi0w"
+const apiInstance = new Brevo.TransactionalEmailsApi();
+
+apiInstance.authentications.apiKey.apiKey =
+    "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";
+
+const sendSmtpEmail = new Brevo.SendSmtpEmail();
+
+sendSmtpEmail.subject = "Test email";
+sendSmtpEmail.textContent = "Zdravo! Ovo je test poruka preko Brevo API-ja.";
+
+
+sendSmtpEmail.sender = {
+    name: "saferchoice",
+    email: "mihajlovicpavle702@gmail.com"
+};
+
+sendSmtpEmail.to = [
+    {
+        email: "gvozdenovicmihajlo772@gmail.com"
     }
-});
+];
 
-transporter.sendMail({
-    from: "mihajlovicpavle702@gmail.com",
-    to: "gvozdenovicmihajlo772@gmail.com",
-    subject: "Test",
-    text: "Ovo je test email preko Brevo-a."
-})
-.then(() => {
-    console.log("EMAIL POSLAT!");
-})
-.catch(error => {
-    console.error("GRESKA:", error);
-});
+apiInstance.sendTransacEmail(sendSmtpEmail)
+    .then(() => {
+        console.log("EMAIL POSLAT!");
+    })
+    .catch((error) => {
+        console.error("GRESKA:", error);
+    });
