29412841bfaf933b83697c4af127e5bc805509d7
89ph88o8gf <mihajlovicpavle702@gmail.com>
2026-08-21 22:42:09 +0200

    Update API key in fetch request

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ async function sendEmail() {
             method: "POST",
             headers: {
                 "accept": "application/json",
-                "api-key": "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",
+                "api-key": "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",
                 "content-type": "application/json"
             },
             body: JSON.stringify({
